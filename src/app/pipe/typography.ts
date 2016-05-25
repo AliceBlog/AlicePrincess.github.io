c643e62df1537df588f929f384db966127b1d0ba
mengzhu <mengzhu@ngs.tech>
2016-05-25 15:58:48 +0800

    change framework

---
+++ src/app/pipe/typography.ts
@@ -0,0 +1,40 @@
+import {Pipe} from '@angular/core';
+
+@Pipe({
+  name:"typography"
+})
+export class typographyPipe {
+transform(value,data){
+  for(var i = 0; i < value.length; i++){
+if(i%2==0){
+  value[i].type="large"
+}
+if(i%2==1){
+  value[i].type="small"
+}
+  }
+  var newValue={"first":[],"second":[],"third":[]};
+
+var colNum=Math.ceil(value.length/3);
+
+for (var i = 0; i < value.length; i++) {
+  if (i<colNum) {
+    newValue.first.push(value[i]);
+
+  }else
+  if (i<colNum*2) {
+    newValue.second.push(value[i]);
+  }else{
+    newValue.third.push(value[i]);
+  }
+}
+if(colNum%2==1){
+  newValue.first[colNum-1].type="small";
+  if(value.length%3==0){
+  newValue.third[colNum-1].type="small";
+  }
+}
+  return newValue[data];
+
+}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,19 @@
-var webpack = require("webpack");
+/**
+ * @author: @AngularClass
+ */
 
-module.exports = {
-  entry: {
-    "vendor": "./app/vendor",
-    "app": "./app/boot"
-  },
-  output: {
-    path: __dirname,
-    filename: "./dist/[name].bundle.js"
-  },
-  resolve: {
-    extensions: ['', '.js', '.ts']
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.ts/,
-        loaders: ['ts-loader'],
-        exclude: /node_modules/
-      },{
-      test: /\.css$/,
-      loaders: ['style', 'css','postcss']
-    },{
-      test: /\.scss$/,
-      loaders: ['style', 'css', 'postcss','sass']
-    },{
-      test: /\.(ttf|eot|woff(2)?)(\?(t=)?[a-z0-9]+)?$/,
-      loader: 'file-loader'
-    }, {
-      test: /\.(svg?)(\?(t=)?[a-z0-9]+)$/,
-      loader: 'file-loader'
-    },{
-			test: /\.svg$/,
-			loader: 'svg-inline'
-		},{
-			test: /\.(jpe?g|png|gif)$/i,
-			loaders: [
-        'url?limit=4000000',
-				'file?hash=sha512&digest=hex&name=[hash].[ext]',
-				'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-			]
-		}]
-  },
-  plugins: [
-    // new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js")
-  ]
+// Look in ./config folder for webpack.dev.js
+switch (process.env.NODE_ENV) {
+  case 'prod':
+  case 'production':
+    module.exports = require('./config/webpack.prod');
+    break;
+  case 'test':
+  case 'testing':
+    module.exports = require('./config/webpack.test');
+    break;
+  case 'dev':
+  case 'development':
+  default:
+    module.exports = require('./config/webpack.dev');
 }
